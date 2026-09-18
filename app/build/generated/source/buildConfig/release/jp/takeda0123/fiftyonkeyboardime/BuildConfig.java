2aea45b1a5bd018fd960dbe1e96a5932fbeade81
0123takeda0123 <0123takeda0123@gmail.com>
2026-09-18 11:44:45 +0900

    Androidアプリ更新

---
+++ app/build/generated/source/buildConfig/release/jp/takeda0123/fiftyonkeyboardime/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package jp.takeda0123.fiftyonkeyboardime;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "jp.takeda0123.fiftyonkeyboardime";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 82;
+  public static final String VERSION_NAME = "1.0";
+}
